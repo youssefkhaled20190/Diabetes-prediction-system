28941a051347e694580a1c2765721f238ae5831f
youssefkhaled207 <64514867+youssefkhaled207@users.noreply.github.com>
2023-03-28 04:06:26 +0200

    auth modified

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 
 Auth = Blueprint("auth", __name__)
 
 
-@Auth.route("/signup")
+@Auth.route("/signup", methods=['GET', 'POST'])
 def _signup():
+
+    if request.method == 'POST':
+        FirstName = request.form.get('firstname')
+        LastName = request.form.get('lastname')
+        Email = request.form.get('email')
+        Password = request.form.get('password1')
+        Re_password = request.form.get('password2')
+        City = request.form.get('city')
+        Country = request.form.get('country')
+        PhoneNumber = request.form.get('phonenumber')
+        Gender = request.form.get('gender')
+
     return render_template("signup.html")
 
 
@@ -14,6 +26,6 @@ def _logout():
     return "<h1> logout </h1>"
 
 
-@Auth.route("/login")
+@Auth.route("/login", methods=['GET', 'POST'])
 def _login():
     return render_template("login.html")
