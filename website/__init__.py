2c7d081f1f2d57820a9a956fcd438a7852990be1
youssifmo <102363668+youssifmo@users.noreply.github.com>
2023-03-30 02:30:54 +0200

    database model

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 
+db=SQLAlchemy()
+DB_NAME="diabetesDatabase.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'DiabetesDetectionWebsite2255ss'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .home import Home
     from .auth import Auth
@@ -12,5 +18,10 @@ def create_app():
     app.register_blueprint(Home, url_prefix='/')
     app.register_blueprint(Auth, url_prefix='/')
     app.register_blueprint(Form, url_prefix='/')
+    
+    
+    
+    with app.app_context():
+        db.create_all()
 
     return app
---
+++ website/models.py
@@ -0,0 +1,37 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    city = db.Column(db.String(100))
+    country = db.Column(db.String(100))
+    phone_number = db.Column(db.String(100))
+    gender = db.Column(db.String(25))
+    
+class Patient(db.model):
+    id=db.Column(db.Integer,primary_key=True)
+    pregnancies=db.Column(db.Integer)  
+    glucose=db.Column(db.Integer)  
+    blood_pressure=db.Column(db.Integer)  
+    skin_thickness=db.Column(db.Integer)  
+    insulin=db.Column(db.Integer)  
+    BMI=db.Column(db.Integer)  
+    diabetes_pedigree_function=db.Column(db.Integer)  
+    age=db.Column(db.Integer)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+    diabetes_app=db.relationship('User')  
+    
+class Doctor(db.model):
+    id=db.Column(db.Integer,primary_key=True)
+    first_name=db.Column(db.String(150))
+    last_name=db.Column(db.String(150))
+    rate=db.Column(db.Float)
+    address=db.Column(db.String(2083))
+    
+
+    
