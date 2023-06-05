9554fbf6dbb0d9ba49f98617d408ea39eb2332a2
youssefkhaled207 <youssefkhaled20190@gmail.com>
2023-06-05 15:55:15 +0300

    - doctor page and some changes in result page
    - doctor page is done (backend and front)
    - result page changes in colors

--- website/doctors.py
+++ website/doctors.py
@@ -1,11 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template ,request
 from flask_login import login_required, current_user
+from .models import Doctor
 
 
 Doctors = Blueprint("doctors", __name__)
 
 
-@Doctors.route("/doctors")
+@Doctors.route("/doctors", methods=['GET','POST'])
 @login_required
 def _Doctors():
-    return render_template("doctors.html", user=current_user, custom_css="doctors")
+ if request.method == 'POST': 
+    select = request.form.get('selector')
+    if select == "all":
+        docdata = Doctor.query.all()
+    else:
+        docdata = Doctor.query.filter(Doctor.address.ilike(f'%{select}%')).all()
+ else:
+    docdata = Doctor.query.all()         
+    
+ return render_template("doctors.html", user=current_user, custom_css="doctors", data=docdata)
